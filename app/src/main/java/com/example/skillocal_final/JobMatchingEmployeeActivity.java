43d60f02ec381b828b233df4168a4acf889790de
basiliodon02-droid <basiliodon02@gmail.com>
2025-11-27 12:20:14 +0800

    added modal for viewing in employee vacancy. but backend has errors.

---
+++ app/src/main/java/com/example/skillocal_final/JobMatchingEmployeeActivity.java
@@ -0,0 +1,12 @@
+package com.example.skillocal_final;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class JobMatchingEmployeeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_job_matching_employee);
+    }
+}
